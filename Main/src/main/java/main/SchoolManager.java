ec66fa7878f21a5a2e73e717f27319be50a1adbc
nicolassampaiorj-tech <2025447@student.cct.ie>
2026-05-23 11:43:55 +0100

    Working the binary tree

--- Main/src/main/java/main/SchoolManager.java
+++ Main/src/main/java/main/SchoolManager.java
@@ -9,5 +9,22 @@ package main;
  * @author Nicolas
  */
 public class SchoolManager {
+    private String name;
+    private Department department;
+    private String title;  // "Principal", "VicePrincipal", "HeadOfDepartment"
     
+    public SchoolManager(String name, Department department, String title) {
+        this.name = name;
+        this.department = department;
+        this.title = title;
+    }
+    
+    public String getName() { return name; }
+    public Department getDepartment() { return department; }
+    public String getTitle() { return title; }
+    
+    @Override
+    public String toString() {
+        return title + " " + name + " (" + department + ")";
+    }
 }
---
+++ Main/src/main/java/main/TreeNode.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class TreeNode {
+    Employee data;
+    TreeNode left, right;
+    
+    public TreeNode(Employee data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
